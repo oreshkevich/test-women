8cc299907c276bff09de3197b1e83970339ea0d2
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-12-06 12:47:06 +0300

    fix: scroll

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ $(window).scroll(function () {
   var sticky = $('.sticky'),
     scroll = $(window).scrollTop();
 
-  if (scroll >= 100) sticky.addClass('fixed');
+  if (scroll >= 600) sticky.addClass('fixed');
   else sticky.removeClass('fixed');
 });
